d045c7529845eb65cfcb29a0dc50388251482cdc
Sebastian Schmidt <publicarray@icloud.com>
2014-09-26 17:02:59 +1000

    splash screen

--- js/model.js
+++ js/model.js
@@ -1,42 +1,57 @@
-var objectId = '815157038515764'
-var appId = '1480115835608916'
-window.fbAsyncInit = function() {
-    FB.init({
-        appId: appId,
-        xfbml: true,
-        version: 'v2.1'
+var objectId = '815157038515764';
+var appId = '1480115835608916';
+// Load the SDK asynchronously
+$(document).ready(function() {
+    $.ajaxSetup({ cache: true });
+    $.getScript('//connect.facebook.net/en_UK/all.js', function(){
+        FB.init({
+          appId: appId,
+          xfbml: true,
+          version: 'v2.1',
+        });
+
+        // process login status
+        loginCallback();
     });
+});
 
-    FB.api('/'+objectId, 'get', function(response) {
-      console.log(response);
-    });
-};
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-
-
-
-
-
-function getAvatarURL(id) {
-    return 'https://graph.facebook.com/' + id + '/picture?type=square';
-};
-
-function APIcall(id) {
-    /* make the API call */
-    FB.api('/{'+id+'}', function(response) {
-        if (response && !response.error) {
-            /* handle the result */
+function loginCallback(){
+    FB.getLoginStatus(function(response) {
+        console.log(response.status);
+        $('#alertText').text(response.status);
+        $('#splash').fadeIn(2000);
+        $('#content').fadeOut(2000);
+        if (response.status === 'connected') {
+            FB.api('/'+objectId, 'get', function(response) {
+                console.log(response);
+                console.log(response.cover.source);
+                console.log(response.description);
+            });
+            testAPI()
+            $('#splash').fadeOut(2000);
+            $('#content').fadeIn(2000);
+        } else {
+            $('#alertText').text('Please Login');
+            $('#alertBody').fadeIn(500);
         }
     });
 }
+
+function testAPI() {
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+    });
+  }
+
+// function getAvatarURL(id) {
+//     return 'https://graph.facebook.com/' + id + '/picture?type=square';
+// };
+
+// function APIcall(id) {
+//      /* make the API call */
+//     FB.api('/' + id, function(response) {
+//         if (response && !response.error) {
+//             /* handle the result */
+//         }
+//     });
+// }
